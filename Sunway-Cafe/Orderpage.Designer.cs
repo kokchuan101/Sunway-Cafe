6b87947bba6902135a4a6ce689c53d61e4b14ce7
KelvinThoo <44352099+KelvinThoo@users.noreply.github.com>
2019-11-24 02:23:09 +0800

    done merging

--- Sunway-Cafe/Orderpage.Designer.cs
+++ Sunway-Cafe/Orderpage.Designer.cs
@@ -183,7 +183,7 @@
             this.Clear1});
             this.dataGridView1.Location = new System.Drawing.Point(652, 96);
             this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(467, 390);
+            this.dataGridView1.Size = new System.Drawing.Size(449, 390);
             this.dataGridView1.TabIndex = 12;
             this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick_1);
             // 
